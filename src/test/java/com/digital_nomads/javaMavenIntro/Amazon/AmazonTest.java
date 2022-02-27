10ac31065133f39d1f0c8cc1bade1420fa3caca8
Belek <belek.muratov@gmail.com>
2022-02-27 12:23:42 +0600

    Thi is Bug Fix

--- src/test/java/com/digital_nomads/javaMavenIntro/Amazon/AmazonTest.java
+++ src/test/java/com/digital_nomads/javaMavenIntro/Amazon/AmazonTest.java
@@ -2,6 +2,6 @@ package com.digital_nomads.javaMavenIntro.Amazon;
 
 public class AmazonTest {
     public static void main(String[] args) {
-        System.out.println("This is amazon test");
+        System.out.println("This is Amazon testssss");
     }
 }
--- src/test/java/com/digital_nomads/javaMavenIntro/Amazon/BudFix.java
+++ src/test/java/com/digital_nomads/javaMavenIntro/Amazon/BudFix.java
@@ -2,6 +2,6 @@ package com.digital_nomads.javaMavenIntro.Amazon;
 
 public class BudFix {
     public static void main(String[] args) {
-        System.out.println("Fixed some bugs");
+        System.out.println("This is budfixxxxxx");
     }
 }
