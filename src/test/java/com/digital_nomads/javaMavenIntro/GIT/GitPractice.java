9f3e2f1e863df39486642fb9618ca9cee4b81f5c
Belek <belek.muratov@gmail.com>
2022-02-27 10:53:27 +0600

    Added Git practice class

---
+++ src/test/java/com/digital_nomads/javaMavenIntro/GIT/GitPractice.java
@@ -0,0 +1,8 @@
+package com.digital_nomads.javaMavenIntro.GIT;
+
+public class GitPractice {
+
+    public static void main(String[] args) {
+        System.out.println("This is git practice");
+    }
+}
