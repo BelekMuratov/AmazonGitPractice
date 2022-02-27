d490844b535934cd74a896f3537b11f62c181b01
Belek <belek.muratov@gmail.com>
2022-02-27 11:58:20 +0600

    Fix/Bugfix 1234

---
+++ src/test/java/com/digital_nomads/javaMavenIntro/Amazon/AmazonTest.java
@@ -0,0 +1,7 @@
+package com.digital_nomads.javaMavenIntro.Amazon;
+
+public class AmazonTest {
+    public static void main(String[] args) {
+        System.out.println("This is amazon test");
+    }
+}
---
+++ src/test/java/com/digital_nomads/javaMavenIntro/Amazon/BudFix.java
@@ -0,0 +1,7 @@
+package com.digital_nomads.javaMavenIntro.Amazon;
+
+public class BudFix {
+    public static void main(String[] args) {
+        System.out.println("Fixed some bugs");
+    }
+}
